1659a5941079497c1b13f0a9d0a354a83be8b3e4
Dmitrii Smertin <dmitrii_smertin@epam.com>
2021-11-29 09:51:19 +0300

    add singleton and config start/finish test

---
+++ src/test/java/test/CommonCondition.java
@@ -0,0 +1,23 @@
+package test;
+
+import driver.DriverSingleton;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class CommonCondition {
+
+    protected WebDriver driver;
+
+    @BeforeMethod()
+    public void setUp() {
+        driver = DriverSingleton.getDriver();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        DriverSingleton.closeBrowser();
+    }
+
+}
